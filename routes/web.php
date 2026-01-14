9620fc8d422e0264adb65a295f4ab91e250ca3e0
Matt Humphrey <git@m4tt.co>
2026-01-14 10:56:53 +0000

    Wrap auth in larger group

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn() => response(null, 444));
 
-Route::name('wealth.')->prefix('wealth')->middleware('auth.basic')->group(function() {
-    Route::get('/', [Controller::class, 'index'])->name('index');
-    Route::post('/update', [Controller::class, 'update'])->name('update');
-    Route::post('/screenshot', [Controller::class, 'screenshot'])->name('screenshot');
+Route::group(['middleware' => 'auth.basic'], function () {
+    Route::name('wealth.')->prefix('wealth')->group(function () {
+        Route::get('/', [Controller::class, 'index'])->name('index');
+        Route::post('/update', [Controller::class, 'update'])->name('update');
+        Route::post('/screenshot', [Controller::class, 'screenshot'])->name('screenshot');
+    });
 });
